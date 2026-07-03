5ccfa5273e8c1585684561b4689de901b1295826
Mikhail Gubenko <83169698+mihaelgubenko@users.noreply.github.com>
2026-07-03 12:14:22 +0300

    1st commit

---
+++ elif_etc.py
@@ -0,0 +1,18 @@
+# age = 18
+# if age >= 18:
+#     print('You are old enough to vote')
+# elif age < 18:
+#     print('You are too young to vote')
+#
+# burgers = 5
+# print(f'{burgers}{'$'}')
+# money = int(input('How much money do you have?'))
+# if money >= 5:
+#     print('You can buy a burger')
+# else:
+#     print('What a shame')
+
+
+age = 12
+res = 'adalte' if 18 > age else 'child'
+print(res)
